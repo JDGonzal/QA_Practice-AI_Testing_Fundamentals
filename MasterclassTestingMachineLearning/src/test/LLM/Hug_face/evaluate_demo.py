2267e0444d4d171d5be07330b5c1e6e7c62b8faf
juan.gonzalezp <jdgonzal2@gmail.com>
2025-08-15 16:55:00 -0500

    MasterclassTestingMachineLearning/05_CommonTraditionalMetrics4ML_Models
      27. Understanding how to compute any score
      28. Ground Truth Table - source of Truth | Test Oracle
      29. Obtain the Prediction table from the LLM - Google Gemini
      30. Machine Learning Metrics - Accuracy
      31. Machine Learning Metrics - Precision
      32. Machine Learning Metrics - Recall
      33. Machine Learning Metrics - F1 Score
      34. Machine Learning Metrics - Perplexity
      35. Demo - Evaluate - Calculate Metrics with Python
      36. Demo - Pytorch - Calculate Perplexity for a Model
      Quiz 2: Chapter Quiz
    
    #MasterclassTestingMachineLearning/05_CommonTraditionalMetrics4ML_Models

---
+++ MasterclassTestingMachineLearning/src/test/LLM/Hug_face/evaluate_demo.py
@@ -0,0 +1,31 @@
+import evaluate
+
+# load the metrics
+accuracy_metrics = evaluate.load("accuracy")
+f1_metrics = evaluate.load("f1")
+precision_metric = evaluate.load("precision")
+recall_metric = evaluate.load("recall")
+
+# Define predictions and references
+predictions = [0, 1, 1, 0, 1]
+references = [0, 1, 0, 0, 1]
+
+# Compute the accuracy
+accuracy_result = accuracy_metrics.compute(
+    predictions=predictions, references=references)
+print("Accuracy:", accuracy_result)
+
+# Compute the F1 score
+f1_result = f1_metrics.compute(
+    predictions=predictions, references=references, average="binary")
+print("F1 score", f1_result)
+
+# Compute the precision
+precision_result = precision_metric.compute(
+    predictions=predictions, references=references, average="binary")
+print("Precision:", precision_result)
+
+# Compute the recall
+recall_result = recall_metric.compute(
+    predictions=predictions, references=references, average="binary")
+print("Recall:", recall_result)
